ce6c18f5cc7897ac52f922af713e20f195fa8c38
Daniel1294 <danielabedrabbo@gmail.com>
2024-10-22 19:05:28 -0500

    x

--- footer.php
+++
@@ -1,7 +0,0 @@
-    <!-- Pie de página -->
-    <footer>
-        <p>&copy; <?php echo date('Y'); ?> PAAK Mecánica Industrial. Todos los derechos reservados.</p>
-    </footer>
-    <?php wp_footer(); ?>
-</body>
-</html>
